b881a8e8c3d1147708d10e836e635ab06ac12c10
MichaelCBrister <70389996+MichaelCBrister@users.noreply.github.com>
2020-10-02 12:14:36 -0500

    completed MVP

--- components/Cards.js
+++ components/Cards.js
@@ -20,3 +20,64 @@
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+
+
+import axios from "axios";
+const cardsContainer = document.querySelector(".cards-container");
+axios.get("https://lambda-times-api.herokuapp.com/articles").then(pass =>{
+    console.log(pass.data.articles)
+    const javascript = pass.data.articles.javascript
+    const bootstrap = pass.data.articles.bootstrap
+    const technology = pass.data.articles.technology
+    const jquery = pass.data.articles.jquery
+    const node = pass.data.articles.node
+    let cardArray = [];
+    javascript.forEach(cardObj => {
+        cardArray.push(cardObj);
+    })
+    bootstrap.forEach(cardObj => {
+        cardArray.push(cardObj);
+    })
+    technology.forEach(cardObj => {
+        cardArray.push(cardObj);
+    })
+    jquery.forEach(cardObj => {
+        cardArray.push(cardObj);
+    })
+    node.forEach(cardObj => {
+        cardArray.push(cardObj);
+    })
+    console.log(cardArray)
+    cardArray.forEach(cardObj =>{
+        const cardElement = cardMaker(cardObj)
+        cardsContainer.appendChild(cardElement);
+    })
+})
+function cardMaker(cardObj){
+    const card = document.createElement("div");
+    const headline = document.createElement("div");
+    const author = document.createElement("div");
+    const imageCont = document.createElement("div");
+    const image = document.createElement("img");
+    const by = document.createElement("span");
+
+    card.appendChild(headline);
+    card.appendChild(author);
+    author.appendChild(imageCont);
+    author.appendChild(by);
+    imageCont.appendChild(image);
+
+    card.classList.add("card");
+    headline.classList.add("headline");
+    author.classList.add("author");
+    imageCont.classList.add("img-container");
+   
+    image.src = cardObj.authorPhoto
+    headline.textContent = cardObj.headline
+    by.textContent = "By " + cardObj.authorName
+ 
+    card.addEventListener("click", event =>{
+        console.log(headline);
+    });
+    return card;
+}
--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,22 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+
+const header = document.querySelector(".header-container")
+function Header() {
+    const headerReturn = document.createElement("div");
+    const date = document.createElement("span");
+    const title = document.createElement("h1");
+    const temp = document.createElement("span");
+    headerReturn.appendChild(date);
+    headerReturn.appendChild(title);
+    headerReturn.appendChild(temp);
+    headerReturn.classList.add("header");
+    date.classList.add("date");
+    temp.classList.add("temp");
+    date.textContent = "MARCH 28, 2020";
+    title.textContent = "Lambda Times";
+    temp.textContent = "98°";
+    return headerReturn;
+}
+header.appendChild(Header());
